9f93019f82a9ff5213cb5616fab422e5c4ffb759
Akansha Roy <akansharoy2008@gmail.com>
2025-12-09 20:33:29 +0530

    env loader script

--- cmd/secret-service/main.go
+++ cmd/secret-service/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"github.com/akansha204/cryptex-secretservice/internal/database"
 	"github.com/akansha204/cryptex-secretservice/internal/routes"
+
+	"github.com/akansha204/cryptex-secretservice/internal/utils"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load(".env")
+	if err := utils.Init(); err != nil {
+		panic(err)
+	}
 
 	database.ConnectDB()
 
